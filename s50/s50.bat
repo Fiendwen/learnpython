78fc1b6bf524d36406930dde167ff108b311851f
silence <553758173@qq.com>
2017-05-17 16:35:13 +0800

    for test

---
+++ s50/s50.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+%~d0
+cd %cd%\
+ ping 192.168.2.1 -n 20>>ping.txt
+
+
+
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               
+                                               
